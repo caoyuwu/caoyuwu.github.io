bb8f6dc2d76b99851c0cad990f037dd19b4b207f
CAOYUWU2020\caoyuwu <caoyuwu@gmail.com>
2022-01-24 18:52:29 +0800

    .

--- tv/plugins/Music163.js
+++ tv/plugins/Music163.js
@@ -14,8 +14,36 @@
    生成的  bZe9V.encText， bZe9V.encSecKey
   测试：
     snadkx-test: Crawl163Music
-*/
+   
+    一百万个可能  music163://29722582
+         163video://B8FD182003B18CFBD4030DDCDCB78372
+         163mv://5307748
+*/ 
+
+function protocol2Type(protocol){
+	switch( protocol ){
+    case "music163": case "163music": return 1;
+    case "163mv":case "mv163": return 2;
+    case "163video": case "video163": return  3;
+	}
+	return -1;
+}
 
 function prepareMediaSource(url,params){
+	return buildLiveUrl(protocol2Type(""+utils.getUrlProtocol(url)),""+utils.getUrlHostAndPath(url));
+}
+
+/**
+@param  type
+      1: Music; 163music:/xxx : https://music.163.com/#/song?id=29722582
+      2: MV;   163mv://xxx  : https://music.163.com/#/mv?id=xxx
+      3: sh视频   163video://B8FD182003B18CFBD4030DDCDCB78372  : https://music.163.com/#/video?id=B8FD182003B18CFBD4030DDCDCB78372
+ */
+function buildLiveUrl( type, id) {
+	print("type="+type+" ; id="+id);
+}
+
+function newRandomStr(){
+	return "J9FsJ9hrKj1VN9S5";
 }
 
--- tv/plugins/QingtingFM.js
+++ tv/plugins/QingtingFM.js
@@ -1,7 +1,19 @@
 /*
 https://www.qingting.fm/radios/XXXXX => qingtingfm://XXXXX
+  CNR中国之声 , qingtingfm://386
 https://www.qingting.fm/radiopage : 生成 播放地址:
 */
 function prepareMediaSource(url,params){
+	  var channel_id =  ""+utils.getUrlHostAndPath(url);
+ //print("====channel_id="+channel_id);	  
+	  var path = "/live/"+channel_id+"/64k.mp3";
+	  var urlEncodedPath = encodeURIComponent(path);//java.net.URLEncoder.encode(path,"UTF-8");
+	  var t0 = utils.currentTimeSeconds()+1*60*60;//utils.System.currentTimeMillis()/1000 + 1*60*60;
+	  var ts = t0.toString(16).toLowerCase();// Long.toHexString(t0).toLowerCase();
+	          //ts = "60a0a496";
+	  var a= "app_id=web&path="+urlEncodedPath+"&ts="+ts;
+	  var  sign = utils. hmacDigestAsHex("HmacMD5","Lwrpu$K5oP",a).toLowerCase();
+		  //StrUtils.toHexString(MessageDigestUtils.hmacDigest("HmacMD5", HmacMD5_Key, a.getBytes(StandardCharsets.UTF_8))).toLowerCase();
+	  return "http://lhttp.qingting.fm"+path+"?app_id=web&ts="+ts+"&sign="+sign;
 }
 
