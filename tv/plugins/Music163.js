a5b2843153357bdac33183f30b4c497def15a755
CAOYUWU2020\caoyuwu <caoyuwu@gmail.com>
2022-01-25 18:06:58 +0800

    .

--- tv/plugins/Music163.js
+++ tv/plugins/Music163.js
@@ -28,10 +28,10 @@ function protocol2Type(protocol){
 	}
 	return -1;
 }
-
 function prepareMediaSource(url,params){
 	return buildLiveUrl(protocol2Type(""+utils.getUrlProtocol(url)),""+utils.getUrlHostAndPath(url));
 }
+
 
 /**
 @param  type
@@ -40,10 +40,95 @@ function prepareMediaSource(url,params){
       3: sh视频   163video://B8FD182003B18CFBD4030DDCDCB78372  : https://music.163.com/#/video?id=B8FD182003B18CFBD4030DDCDCB78372
  */
 function buildLiveUrl( type, id) {
-	print("type="+type+" ; id="+id);
+	//print("type="+type+" ; id="+id);
+	var url,params ;//= new java.util.LinkedHashMap<>();
+    //i2x={"id":"29722582","ids":"[\"29722582\"]","limit":10000,"offset":0,"csrf_token":""}
+    switch( type ) {
+        case 1: // 163music
+	        url = "https://music.163.com/weapi/song/enhance/player/url";
+	        params = {
+	        	ids : "[" + id + "]",//JsonUtils.encode(new String[] {id}, 1));
+	        	"br": 128000
+	        };
+	        break;
+        case 2: //163mv
+	        url = "https://music.163.com/weapi/song/enhance/play/mv/url";
+	         params = {
+	        		 id: id,
+	        		 r:1080
+	         };
+	        break;
+        case 3: // 163video
+	        url = "https://music.163.com/weapi/cloudvideo/playurl";
+	        params = {
+	        		"ids":"[\""+id+"\"]",
+	        		resolution:720
+	        };
+            break;
+        default:
+        	throw "type="+type;
+    }
+    params.csrf_token = "";
+    var jsonEncodedParams = JSON.stringify(params);//, 1);
+ //print("jsonEncodedParams="+jsonEncodedParams);
+    var postParams = newHttpPostParams(jsonEncodedParams);
+  //  print(postParams.encText);
+  //  print(postParams.encSecKey);
+    //HttpFormOutput formOutput = new HttpFormOutput(new LinkedHashMap<String,String>());
+    formParams = {	
+    		params: postParams.encText,
+    		encSecKey:postParams.encSecKey
+    };
+    //https://music.163.com/weapi/song/enhance/player/url
+
+    //WebContent c = HttpUtils.post("https://music.163.com/weapi/song/enhance/player/url", formOutput);
+    //byte retData[] = HttpUtils.post("https://music.163.com/weapi/song/enhance/player/url",formParams);
+    // https 报错，先使用 http
+    //final String url = "https://music.163.com/weapi/song/enhance/player/url";
+    //var retData = HttpUtils.post(url,formParams);
+            //System.out.println(c.getContentAsString());
+    var retText = utils.httpPostAsString(
+    		url,
+    		null,
+    		"application/x-www-form-urlencoded; charset=utf-8",
+    		formParams,
+    		0x400
+    	);
+ print("retText="+retText);   
+    var m = JSON.parse(retText);
+    var objData = type==3?m.urls:m.data; //m.get(type==3?"urls":"data");
+    //final java.util.Map<String,Object> data;
+    if( objData instanceof Array ){
+        if( objData.length==0 )
+            return null;
+        data = objData[0];
+    } else
+        data = objData;
+    return data==null ? null : data.url;
+    
 }
 
-function newRandomStr(){
+var AES_IV = "0102030405060708";
+var EK_3 = "0CoJUm6Qyw8W8jud";
+/*
+ * EncSecKey 是用 RSA( publicKey={EK_2,EK_1}
+ */
+/*
+ * @param String params 
+ */
+var EncSecKey="c7399b736dd9cb241a1c8b5b66325b059eda5eea6654b851696b7a4f8cc9fc4135be6f0acb506a666275b48ded191b9c4cc0067594e8f80c3ac3475bf79d87cbc16946955c67b7d10010f4d24fad7b119c6e78b017b2d42bafc9b4f9c5e5675738ca8d7499f060df4ce82b1d4b4480bcd73e9a0cde5a365d4c100d36fc7b17ec";
+function newHttpPostParams(params){
+	var randomKey = newRandomStr(16);
+	var aesEncodeParams = utils.aesEncode(AES_IV,EK_3,params);
+    //  System.out.format("EK_3(%s) - 加密后：%s \n",EK_3,aesEncodeParams);
+         aesEncodeParams = utils.aesEncode(AES_IV,randomKey,aesEncodeParams); //J9FsJ9hrKj1VN9S5
+      return {
+    	  encText : aesEncodeParams,
+    	  encSecKey : EncSecKey
+      };
+}
+
+function newRandomStr(len){
 	return "J9FsJ9hrKj1VN9S5";
 }
 
--- tv/plugins/QQ.js
+++ tv/plugins/QQ.js
@@ -3,5 +3,170 @@ https://y.qq.com/n/ryqq/songDetail/XXXXX => qqmusic/XXXX
 https://y.qq.com/n/ryqq/mv/XXXXX => qqmv://XXXXX
 */
 
+function uuidGenerate(){
+	var minLen = 10, maxLen = 16; 
+	var ranLen = minLen + Math.floor(Math.random()*(maxLen-minLen));
+	var dir = "0123456789abcdefghijklmnopqrstuvwxyz";
+	var a = [];
+	for( var i=0;i<ranLen;i++){
+		a.push(dir[Math.floor(Math.random()*dir.length)]);
+	}
+	return a.join("");
+}
+
+function protocol2Type(protocol){
+	switch( protocol ){
+    case "qqmusic":  return 1;
+    case "qqmv": return 2;
+	}
+	return -1;
+}
+
 function prepareMediaSource(url,params){
+	return buildUrl(protocol2Type(""+utils.getUrlProtocol(url)),
+			""+utils.getUrlHostAndPath(url));
+}
+
+var encNonce="CJBPACrRuNy7";
+var signPrxfix="zza";
+
+function genSign( postBody){
+    return signPrxfix+uuidGenerate()+
+            utils.md5Hex(encNonce+postBody).toLowerCase()
+            ;
+    ///Md5Encrypt.convertToMd5(encNonce+encParams);
+}
+
+
+var _UIN = 908377347;
+
+function buildReqBody(type,songId){
+    comm = {
+    cv:4747474,
+    ct:24,
+    format:"json",
+    inCharset:"utf-8",
+    outCharset:"utf-8",
+    notice:0,
+    platform:"yqq.json",
+    needNewCode:1,
+    uin:""+utils.getConfigPreference("QQ.UIN",_UIN), //QQ 👌908377347;
+    g_tk_new_20200303:1390962086,
+    g_tk:1390962086
+    };
+    return {
+    	comm: comm,
+    	req_1: buildReq1(type,songId)
+    }
+}
+
+var URL0 = "https://dl.stream.qqmusic.qq.com/";
+var apiServer = [
+        "https://u.y.qq.com/cgi-bin/musicu.fcg",
+        "https://u.y.qq.com/cgi-bin/musics.fcg"
+];
+
+function buildUrl( type, songId) {
+	print("type="+type+" ; songId="+songId);
+	var postBody = JSON.stringify(buildReqBody(type,songId));
+	var url = apiServer[Math.floor(Math.random()*apiServer.length)]
+    			+"?_="+utils.currentTime()
+    			+"&sign="+genSign(postBody)//output.getContentAsString())
+    			;
+//HttpUtils.appendUrlParameters(,urlParams);
+	var headers = {
+			//headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4512.4 Safari/537.36");
+			"Referer": "https://y.qq.com/"
+	};
+	var sessionId = utils.getConfigPreference("QQ.SESSIONID");
+	if ( sessionId!=null) {
+		headers.Cookie = "fqm_sessionid=" + sessionId;
+	}
+	var text = utils.httpPostAsString(
+			 url,
+			 headers,
+			 "application/json;charset=utf-8",
+			 postBody,
+			 0x400
+			);
+print("返回："+text);	
+	var m = JSON.parse(text);
+	if( m.code!=0 || !m.req_1 ) {
+		return null;
+	}
+	return parseUrlFromResponse(type,m.req_1.data,songId);
+}
+
+function buildReq1(type,songId){
+	switch( type ){
+	case 1:
+	{
+		var req1_Params = {
+			songmid:[songId],
+		    songtype:[0],
+		    uin:""+utils.getConfigPreference("QQ.UIN",""+_UIN),  // QQ 👌908377347;
+		    loginflag:1,
+		    platform:"20"	
+		};
+		var GUID = utils.getConfigPreference("QQ.GUID");
+		if( GUID ){
+			req1_Params.guid = ""+GUID;
+		};
+		return {
+			module:"vkey.GetVkeyServer",
+	        method:"CgiGetVkey",
+	        param: req1_Params
+		};
+	}
+	case 2:
+	{
+		var req1_Params = {
+			vids:[songId],
+		    request_typet:10001,
+		    addrtype:3,
+		    format:264
+		};
+		return {
+			module:"gosrf.Stream.MvUrlProxy",
+	        method:"GetMvUrls",
+	        param: req1_Params
+		};
+	}
+	default:
+		throw "type="+type;
+	}
+}
+function parseUrlFromResponse(type,data,songId){
+	switch( type ){
+	case 1: // music
+	{
+		var midurlinfo = data ? data.midurlinfo : null;
+        var midurlinfo0 = midurlinfo && midurlinfo.length>0 ? midurlinfo[0] : null;
+        var purl = midurlinfo? midurlinfo0.purl : null;
+        return purl==null || purl=="" ? null : URL0+purl;
+	}
+	case 2:{
+		var vdata = data ? data[songId] : null;
+		if( vdata==null )
+			return null;
+        var vtypes = ["hls","mp4"]
+		// vdata.hls = [{freeflow_url:},...]
+        for(var iVType=0;iVType<vtypes.length;iVType++ )
+        {
+            var vurls = vdata[vtypes[iVType]];
+            if(  !(vurls instanceof Array)) {
+                break;
+            }
+            for(var j=0;j<vurls.length;j++ ) {
+                var freeflow_url = vurls[j].freeflow_url;
+                if( freeflow_url && freeflow_url.length>0 ) {
+                    return freeflow_url[0];
+                }
+            }
+        }
+        return null;
+	}
+	default:
+		throw "type="+type;
+	}
 }
