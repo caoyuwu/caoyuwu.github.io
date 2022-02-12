5f03cdb521bbe5da4138838e7c71e767e3a74df9
caoyuwu@gmail.com <caoyuwu@gmail.com>
2022-02-12 22:02:52 +0800

    .

--- tv/plugins/Jsyl.js
+++ tv/plugins/Jsyl.js
@@ -1,18 +1,23 @@
 /*
+
 */
+var JSYL_token = "d79709285951d7c9fd4ec36ac783979e";
+var JSYL_accessToken = "c515f632-5858-4b99-86b3-c20a3c0ecb2f";
+var JSYL_authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIzOTE0NDM2OSIsInVzZXJuYW1lIjoiKzEzNjM5MTQwNSIsInJvbGUiOjAsImxvYiI6MSwiaWF0IjoxNjQ0NTk2MTMwLCJleHAiOjE2NDcxODgxMzB9.M7XxhH8Boy_bs2ZVGXNMjF0c9u5LtS-nDjcv6Kz3ANuntLFFrsQg3igBSOYrqYD5n3KbH5EHIyJhFci3-N6ZGQ";
+var JSYL_liveButter2 = "YxE4NA+07NjzfqbSEcFMk+QGYlRdlFydY0q8Cbq6W4fKDuqCjw2bj3wSpASCBMN6lR6mSQBc66LfkTzEWe2KyHuQephq1P4difqyiuUrYXtxvlSxBvbeKy6I9GfOQgtAmOZm4Uw1uRRBVODVQfTDlsA8jLo61xhVEvQBSMBF9Ol5VMwaG26VKONqmXUn+y6z4mwQxFBb20nxHBmmxvBWdniIsX/FPwQI/mafnfp8AdI=";
 
 function prepareMediaSource(url,params){
   var userId = utils.getUrlHostAndPath(url);
   var params = {
-     "token":utils.getConfigPreference("JSYL.token"),
+     "token":utils.getConfigPreference("JSYL.token",JSYL_token),
      "uid":userId
   };
-  var authToken = utils.getConfigPreference("JSYL.authToken");
+  var authToken = utils.getConfigPreference("JSYL.authToken",JSYL_authToken);
   var headers = {
-	    "access-token":utils.getConfigPreference("JSYL.accessToken"),
+	    "access-token":utils.getConfigPreference("JSYL.accessToken",JSYL_accessToken),
 	    "jwt-token":authToken,
 	    "Authorization": "Bearer "+authToken,
-	    "X-Live-Butter2":utils.getConfigPreference("JSYL.liveButter2"),
+	    "X-Live-Butter2":utils.getConfigPreference("JSYL.liveButter2",JSYL_liveButter2),
 	    "times":new Date().getTime(),
 	    "platform":Platform,
 	    "app-version":AppVersion,
@@ -42,10 +47,10 @@ function loadMenus(path,params){
   var p = path.indexOf("/");
   var type = p<0 ? path : path.substring(0,p);
   var page = p<0 ? 1 : parseInt(path.substring(p+1));
-  var authToken = utils.getConfigPreference("JSYL.authToken");
+  var authToken = utils.getConfigPreference("JSYL.authToken",JSYL_authToken);
   var params = {
 				"type": type,
-				"flowToken": utils.getConfigPreference("JSYL.token"),
+				"flowToken": utils.getConfigPreference("JSYL.token",JSYL_token),
 				"latitude":"","latitude":"",
 				"page": page,
 				"size": 50
@@ -53,7 +58,7 @@ function loadMenus(path,params){
 				//,"t",new js.Date().getTime()
 		};
 	var headers = {
-	    "access-token":utils.getConfigPreference("JSYL.accessToken"),
+	    "access-token":utils.getConfigPreference("JSYL.accessToken",JSYL_accessToken),
 	    "jwt-token":authToken,
 	    "times":new Date().getTime(),
 	    "platform":Platform,
