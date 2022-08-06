72fa05beae66318cc20890580abae96fe8f67279
caoyuwu@gmail.com <caoyuwu@gmail.com>
2022-08-06 18:28:30 +0800

    .

--- tv/plugins/4GTV.js
+++ tv/plugins/4GTV.js
@@ -1,10 +1,57 @@
 /*
  https://www.4gtv.tv/channel
+ 4gtv://xxx 
+ 
+ 参考: http://kjol.cc/4gtv真实视频获取.html/comment-page-1
 */
 
+var AES_Key = "ilyB29ZdruuQjC45JhBBR7o2Z8WJ26Vg";
+var AES_IV = "JUMxvVMmszqUTeKn";
+ 
 
 function prepareMediaSource(url,params){
 	var cid = utils.getUrlHostAndPath(url); //"" +
+	/*
+	https://api2.4gtv.tv/Channel/GetChannel/31
+	*/
+	var text = utils.httpGetAsString("https://api2.4gtv.tv/Channel/GetChannel/"+cid,null,0x80|0x400);
+	// 0x400 : 加 WebSearchUtils.getRequestHeaders
+	var retVal = JSON.parse(text);
+	var fs4GTV_ID = retVal.Data ? retVal.Data.fs4GTV_ID : null;
+	//print("fs4GTV_ID="+fs4GTV_ID);
+	if( !fs4GTV_ID )
+	   return null;
+	   
+	var value = '{"fnCHANNEL_ID":1,"fsASSET_ID":"'+fs4GTV_ID+'","fsDEVICE_TYPE":"pc","clsIDENTITY_VALIDATE_ARUS":{"fsVALUE":""}}';    
+//	var evalue = utils.aesEncode(AES_IV,AES_Key,value);
+	//print("evalue="+evalue);
+	var postData = {
+	  "value" : utils.aesEncode(AES_IV,AES_Key,value)
+	};
+	var text = utils.httpPostAsString(
+   	  "https://api2.4gtv.tv/Channel/GetChannelUrl3",
+   	  null,
+   	  "application/json;charset=utf-8",
+   	  postData,
+   	  0x80|0x400
+   );
+   //print("text="+text);
+   retVal = JSON.parse(text);
+   if( !retVal.Data )
+     return null;
+   text = utils.aesDecode(AES_IV,AES_Key,retVal.Data); 
+  // print("text="+text);
+   retVal = JSON.parse(text);
+   var flstURLs = retVal.flstURLs;
+   if( !flstURLs || flstURLs.length==0 )
+     return null;
+    for(var i=0;i<flstURLs.length;i++){
+       var s = flstURLs[i];
+       if( s.indexOf(".4gtv.tv")>0 ){
+       	 return s;
+       }
+    } 
+    return flstURLs[0];
 }
 
 
