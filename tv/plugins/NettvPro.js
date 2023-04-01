6743e6b4c2d91761e61490cd402b66340498ed82
caoyuwu@gmail.com <caoyuwu@gmail.com>
2023-04-01 23:07:38 +0800

    .

---
+++ tv/plugins/NettvPro.js
@@ -0,0 +1,19 @@
+/*
+  https://www.nettvpro.live/
+*/
+
+/*
+   nettvpro://hongkong/146 => view-source:https://www.nettvpro.live/hongkong/146.html
+*/
+function prepareMediaSource(url,params){
+	var videoId = utils.getUrlHostAndPath(url);
+}
+
+/*
+  
+  nettvpro-list:Asia/china => view-source:https://www.nettvpro.live/Asia/china/
+  nettvpro-list:hongkong => view-source:https://www.nettvpro.live/hongkong/
+  
+*/
+function loadMenus(path,params){
+}
