8a0f03821fa6b70846b9e3b3830d1bb0f405d7fd
THINKBOOK\caoyu <caoyuwu@snsoft.com.cn>
2023-12-17 21:46:22 +0800

    .

--- tv/plugins/Haoqu99.js
+++ tv/plugins/Haoqu99.js
@@ -8,6 +8,15 @@ http://tv.haoqu99.com/2/zhejiangweishi.html
     
 */
 var HOMEURL = "http://tv.haoqu99.com/";
+
+function loadUrls(url,params)
+{
+}
+function prepareMediaSource(url,params){
+  var urls = loadUrls(url,params);
+  return urls && urls.length>0 ? urls[0].url : null;
+}
+
 function loadMenus(url,params){
 	var path = utils.getUrlHostAndPath(url);
        
@@ -24,12 +33,12 @@ function loadMenus(url,params){
     if( path=="") {
 		//var ea = doc.getBody().querySelectorAll(" > body " );
 		//print(ea.length);
-		var ulSelector1 = " > body > div#bd > div.filter > div.bd >  dl.filter-area  ul.filter-list > li a";
-		var ea = doc.getBody().querySelectorAll(ulSelector1);
+		var ulSelector = " > body > div#bd > div.filter > div.bd >  dl.filter-area  ul.filter-list > li a";
+		var ea = doc.getBody().querySelectorAll(ulSelector);
 	//print(ea.length);	
 		for(var i=0;i<ea.length;i++){
 			var href = ea[i].getAttribute("href");
-			print(href);
+			//print(href);
      	    if( !href  )  
      	        continue;
      	    var title = ea[i].getAllNodeValue(); 
@@ -42,5 +51,18 @@ function loadMenus(url,params){
 	/*
 	  
 	*/
+	var qSelector = " > body > div#bd > div.col-sya.p-cols > div.p-col1  div.channel-list > div.bx-sya > div.bd";
+	var ea = doc.getBody().querySelectorAll(qSelector);
+	for(var i=0;i<ea.length;i++){
+		var headE = ea[i].querySelector(">div > h3");
+		var items = [];
+		a.push({items:items,title:headE.getAllNodeValue()});  
+		var liA = ea[i].querySelectorAll(">div >ul > li > a");
+		for(var j=0;j<liA.length;j++){
+			var href = liA[j].getAttribute("href");
+			var title = liA[j].getAllNodeValue(); 
+			items.push({url:"@haoqu99-urls:"+href,title:title.trim()});  
+		}
+	}
 	return a; 
 }
