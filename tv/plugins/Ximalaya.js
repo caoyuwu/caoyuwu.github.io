37f7f633454459d7d7e29be6db3b75c9cefc85c7
THINKBOOK\caoyu <caoyuwu@snsoft.com.cn>
2024-02-18 19:08:35 +0800

    .

--- tv/plugins/Ximalaya.js
+++ tv/plugins/Ximalaya.js
@@ -2,15 +2,17 @@
  https://www.ximalaya.com/radio/93 
  =>https://live.ximalaya.com/live-web/v1/radio?radioId=93
   ximalayaradio://93
+  https://live.xmcdn.com/live/" + radioId + "/64.m3u8?transcode=ts"
 */
 
 function prepareRadioSource(radioId){
+	// https://live.xmcdn.com/live/" + radioId + "/64.m3u8?transcode=ts"
 	var text = utils.httpGetAsString("https://live.ximalaya.com/live-web/v1/radio?radioId="+radioId);
 	var data = JSON.parse(text);
-	if(data.ret!=0)
+	if(data.ret)
 		 throw data.msg;
 	//aac64
-	playUrl = data.data.playUrl;
+	var playUrl = data.data.playUrl;
 	return playUrl.ts64 || playUrl.aac64 || playUrl.ts24 || playUrl.aac24;
 }
 
