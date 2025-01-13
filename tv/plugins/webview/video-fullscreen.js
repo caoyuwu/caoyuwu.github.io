522d4642bae5f9cc58abaf3a8cdcfcc4c1e15c34
caoyuwu@snsoft <caoyuwu@snsoft.com.cn>
2025-01-13 19:43:18 +0800

    .

--- tv/plugins/webview/video-fullscreen.js
+++ tv/plugins/webview/video-fullscreen.js
@@ -1,5 +1,6 @@
  var videos = document.getElementsByTagName("video");
- alert("执行到 video-fullscreen.js : videos.length="+videos.length);
+ console.log("执行到 video-fullscreen.js : videos.length="+videos.length);
+ //alert("执行到 video-fullscreen.js : videos.length="+videos.length);
  if (videos.length>0){
 	 videos[0] .requestFullscreen();
  }
