e0ca02547dfb8a725cad617739ac0d1eb4f3fbeb
caoyuwu@gmail.com <caoyuwu@gmail.com>
2025-01-18 15:29:30 +0800

    .

--- tv/plugins/CCTV-HttpService.js
+++ tv/plugins/CCTV-HttpService.js
@@ -2,9 +2,11 @@
 /*
 https://js.player.cntv.cn/creator/h5.worker?v=220805
 http://m.lan:8803/cctv-httpservice/info
+http://m.lan:8803/cctv-httpservice/ldncctvwbcdtxy.liveplay.myqcloud.com/ldncctvwbcd/cdrmldcctv13_1/index.m3u8?b=200-2100
 */
 
 //(function(){
+/*	
   //var s = utils.httpGetAsString("https://js.player.cntv.cn/creator/h5.worker?v=220805",0x408);
   var s = utils.httpGetAsString("http://172.20.0.20/temp/cctv-h5.worker.js",0x408);
   utils.debug("CCTV-HttpService","s.length="+s.length);
@@ -15,9 +17,14 @@ http://m.lan:8803/cctv-httpservice/info
   delete (s);
   var CNTVH5PlayerModule = CNTVModule();
   //CNTVModule
+  */
 //})()
 function httpService(params){
 	var uri = params.uri;
+	/*
+	https://ldncctvwbcdtxy.liveplay.myqcloud.com/ldncctvwbcd/cdrmldcctv13_1/index.m3u8?b=200-2100
+	/cctv-httpservice/ldncctvwbcdtxy.liveplay.myqcloud.com/ldncctvwbcd/cdrmldcctv13_1/index.m3u8?b=200-2100
+	*/
 	var p = uri.indexOf("/",1);
 	var path = p>0 ? uri.substring(p+1) : "";
 	if( path=="" || path=="info" ){
@@ -29,4 +36,30 @@ function httpService(params){
 		content : text
 		};
 	}	
+	/*
+	path = "ldncctvwbcdtxy.liveplay.myqcloud.com/ldncctvwbcd/cdrmldcctv13_1/index.m3u8?b=200-2100"
+	*/
+	var suffix = getUrlSuffix(path);
+	if( suffix=="m3u8" ){
+		return httpService4M3zU8("https://"+path);
+	}
+	return {
+		    status : 404,
+			contentType : "text/plain;charset=utf-8",
+			content : "Not found"
+		};
+}
+
+function httpService4M3zU8(url){
+	return {
+				contentType : "text/plain;charset=utf-8",
+				content : url
+	};
+}
+
+function getUrlSuffix(url){
+	var p = url.indexOf("?");
+	if( p>0 )url = url.substri(0,p) ;
+	p = url.lastIndexOf('.');
+    return p>0 ? path0.substring(p+1) : null;
 }
