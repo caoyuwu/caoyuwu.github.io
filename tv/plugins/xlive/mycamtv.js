92d9d755e19b89a4abb57f422538648a0cc40cd6
caoyuwu@gmail.com <caoyuwu@gmail.com>
2025-03-19 23:41:26 +0800

    .

--- tv/plugins/xlive/mycamtv.js
+++ tv/plugins/xlive/mycamtv.js
@@ -13,18 +13,43 @@
 		//items : "@crawler-list://xvideo/mycamtv.js#List2?[PATH=${URLDOM.attr.href}]"
 		loadItems: 	function(items,_contentUrl,contentCache,macros){
 			items.push(
-				{title:"推荐",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/recommended]"},
-				{title:"中文",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/chinese]"},
+				//				女主播 情侣	男主播 跨性別
+				{title:"女主播",items:[
+					{title:"推荐",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/recommended]"},
+					{title:"中文",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/chinese]"},
 					{title:"日语",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/japanese]"},
-					{title:"顶级免费现场视频",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/popular]"},
-					{title:"情侣直播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/couples]"},
-					{title:"新 主播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/new]"},
+					{title:"新主播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/new]"},
 					{title:"VR直播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/girls/vr]"}
-					
-						);
+				   ]
+				  },
+				  {title:"情侣",items:[
+  					{title:"推荐",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/couples/recommended]"},
+  					{title:"中文",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/couples/chinese]"},
+  					{title:"日语",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/couples/japanese]"},
+  					{title:"新主播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/couples/new]"},
+  					{title:"VR直播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/couples/vr]"}
+				  				   ]
+				  				  },
+				  {title:"男主播",items:[
+  					{title:"推荐",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/men/recommended]"},
+  					{title:"中文",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/men/chinese]"},
+  					{title:"日语",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/men/japanese]"},
+  					{title:"新主播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/men/new]"},
+  					{title:"VR直播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/men/vr]"}
+				  				   ]
+				  				  },	
+				  {title:"跨性別",items:[
+	  					{title:"推荐",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/trans/recommended]"},
+	  					{title:"中文",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/trans/chinese]"},
+	  					{title:"日语",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/trans/japanese]"},
+	  					{title:"新主播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/trans/new]"},
+	  					{title:"VR直播",items:"@crawler-list://xvideo/mycamtv.js#List2?[PATH=/trans/vr]"}
+				  				   ]
+				  				  }			  			  
+				);
 		}
 	},
 	List2: {
 		contentUrl : "~${PATH}",
-		}
+	}
 }
