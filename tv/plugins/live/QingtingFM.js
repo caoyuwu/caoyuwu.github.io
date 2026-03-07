a2f5a2d105e9ed7e927f10a1e2c346a0896e2c61
caoyuwu@gmail.com <caoyuwu@gmail.com>
2026-03-07 20:48:35 +0800

    .

--- tv/plugins/live/QingtingFM.js
+++ tv/plugins/live/QingtingFM.js
@@ -48,12 +48,30 @@ https://api-v3.tingtingfm.com//recommend/index_v3_6?
 			//print("QingtingFM.loadItems : contentUrl = "+contentUrl+",version="+this._parent.version+","+this._parent.client);
 			var params = "version="+this._parent.version+"&client="+this._parent.client;
 			print("params = "+params+",sign = "+ this._parent.genSign(params));
-			var url = "https://api-v3.tingtingfm.com//recommend/index_v3_6?"+params+"&api_sign="+this._parent.genSign(params);	
-			print("url="+url);
+			var url = "https://api-v3.tingtingfm.com/recommend/index_v3_6?"+params+"&api_sign="+this._parent.genSign(params);	
+//			print("url="+url);
 			var json = utils.httpGetAsString(url,0x408);
-			print("json="+json);
+//			print("json="+json);
 			var m = JSON.parse(json);
 			//var bj_radio = m.data.bj_radio;
+			/*
+			{
+			               "content": "yWRo5nxaEA",
+			               "anchor": "\u6559\u80b2\u9762\u5bf9\u9762BRTV",
+			               "live_status": 4,
+			               "live_num": 141,
+			               "file_url": "https:\/\/ttfm2018pub-oss-cdn.tingtingfm.com\/cover\/2020\/0318\/d4\/cb\/d4cbadaaa1fd74c78d64a7f29f9a6836.jpg?x-oss-process=image\/resize,h_800,w_800",
+			               "title": "\u5403\u900f\u62db\u8003\u65b0\u653f\uff0c\u5408\u7406\u89c4\u5212\u5fd7\u613f\u586b\u62a5",
+			               "notice_st": 1584610200,
+			               "s_date": 1584610202,
+			               "type": 3
+			           },
+			*/
+			if(m.data.live) for(var i of m.data.live){
+				var title =  "直播-"+i.anchor+"-"+i.title;
+				var id = i.content;
+				items.push({title:title,url:"crawler:live/QingtingFM.js;live:"+id});
+			}			
 			/*
 			{
 							"content": "R6gayOdeVM",
@@ -66,34 +84,74 @@ https://api-v3.tingtingfm.com//recommend/index_v3_6?
 							"fm": "FM97.4"
 			}
 			*/
-			for(var i of m.data.bj_radio){
-				var title = i.radio_name+"-"+i.program_name;
+
+			if(m.data.bj_radio) for(var i of m.data.bj_radio){
+				var title = "广播-"+i.radio_name+"-"+i.program_name;
 				var id = i.content;
 				//print(title+":"+id);
 				//https://mobile.tingtingfm.com/v3/fm/EOAonLJy7G //https://mobile.tingtingfm.com/v3/fm/"+
-				items.push({title:title,url:"crawler:live/QingtingFM.js;"+id});
+				items.push({title:title,url:"crawler:live/QingtingFM.js;radio:"+id});
 			}
 		}
 	},
 	"MediaSource":{
 		/*
+	声音：	
 		https://api-v3.tingtingfm.com//broadcast/get_radio_programs?
 		 version=h5_5.36.4
 		 &client=h5_1FCCtb8Y6Kb9iEuLmXVCdgJX9dfcvy
 		 &h_radio_id=R6gayOdeVM&api_sign=556d5c0c2ebabbc7d13efd702ef06879
+	视频：	 
+		 https://api-v3.tingtingfm.com/live/get_live
+		 参数：
+		 version=h5_5.36.4
+		 client=h5_1FCCtb8Y6Kb9iEuLmXVCdgJX9dfcvy
+		 h_live_id=4jaEKEPd9A
+		 api_sign=517e31bb5b2fa4b68a5c2c36d23dfbf3
+	返回：	 
+		 https://mobile.tingtingfm.com/v3/video_chatroom/4jaEKEPd9A?type=1
+		   	 
 		*/
-		getUrl : function(radioId,macros){
-			print("radioId="+radioId+" ;"+macros);
+		getUrl : function(contentId,macros){
+			print("contentId="+contentId+" ;"+macros);
 			if( !this._parent.client ){
 			    this._parent.client = this._parent.genClientVal();
 			}
-			var params = "version="+this._parent.version+"&client="+this._parent.client+"&h_radio_id="+radioId;
-			var url = "https://api-v3.tingtingfm.com//broadcast/get_radio_programs?"+params+"&api_sign="+this._parent.genSign(params);
-			print("url="+url);
-			var json = utils.httpGetAsString(url,0x408);
-			print("json="+json);
-			var m = JSON.parse(json);
-			return m.data.info.play_urls.hls || m.data.info.play_urls.flv;
+			var p0 = contentId.indexOf(":");
+			if( p0<0 ) return ;
+			var type = contentId.substring(0,p0);
+			contentId = contentId.substring(p0+1);
+			if( type=="radio" ){
+				var params = "version="+this._parent.version+"&client="+this._parent.client+"&h_radio_id="+contentId;
+				var url = "https://api-v3.tingtingfm.com//broadcast/get_radio_programs?"+params+"&api_sign="+this._parent.genSign(params);
+			//	print("url="+url);
+				var json = utils.httpGetAsString(url,0x408);
+			//	print("json="+json);
+				var m = JSON.parse(json);
+			   return m.data.info.play_urls.hls || m.data.info.play_urls.flv;
+			}
+			if( type=="live" ){
+				/*				version
+				h5_5.36.4 clienth5_1FCCtb8Y6Kb9iEuLmXVCdgJX9dfcvy&
+				h_live_id
+				DOdGK6Dd34
+				api_sign
+				b9aaa309feebcbad0052c1bc35a124cf
+				*/
+				var params = "version="+this._parent.version+"&client="+this._parent.client+"&h_live_id="+contentId;
+				var url = "https://api-v3.tingtingfm.com/live/get_live?"+params+"&api_sign="+this._parent.genSign(params);
+				var json = utils.httpGetAsString(url,0x408);
+				//		if(_debug)	print("json="+json);
+				var m = JSON.parse(json);		
+				var pull_url = m.data.pull_url;
+				if( !pull_url ) return null;
+		//https://pull2push-2.tingtingfm.com/ttfmrelease/fenghuiluzhuan_720p.m3u8?auth_key=1772973186-gxv4kA7S-0-3e006b125b9e52c6efa5c93056ce4212		
+				var urlInfo = pull_url.hls || pull_url.flv || pull_url.rtmp;
+				if( urlInfo  ){
+					return urlInfo.url+"?auth_key="+urlInfo.ak;
+				}
+				return m.data.backrecord_url_mp4;
+			}
 		}
 		
 		
